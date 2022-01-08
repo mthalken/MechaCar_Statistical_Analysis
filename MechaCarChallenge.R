f92513d55e48fee80ff56bf98d74532c5d528baf
Mike Thalken <mthalken53@gmail.com>
2022-01-08 12:32:27 -0800

    Adding RScript of MechaCar_mpg.csv

---
+++ MechaCarChallenge.R
@@ -0,0 +1,27 @@
+# Load libraries
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+
+# Read in data
+mecha_data <- read.csv('data/MechaCar_mpg.csv', check.names = F,stringsAsFactors = F)
+
+lm()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
